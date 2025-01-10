5e656fdec9b70569e6ef88b6c24a75f71f0425c8
Preet Vadaliya <preetvadaliya@gmail.com>
2025-01-10 23:55:05 +0100

    refactor: Move main files to src directory and update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import type React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
+      <Text>Hello World</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
