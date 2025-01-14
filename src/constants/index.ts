fed4215a30646a3759a451eea22d7e6075bbec62
Preet Vadaliya <preetvadaliya@gmail.com>
2025-01-14 17:02:07 +0100

    feat: Remove unused SQL table creation logic for products

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,3 @@ export enum Screens {
   LIST_QUOTES = 'LIST Quotes',
   CREATE_QUOTE = 'Create Quote'
 }
-
-export const PRODUCT_TABLE_SQL = `
-CREATE TABLE IF NOT EXISTS products (id TEXT PRIMARY KEY NOT NULL, title TEXT, price REAL NOT NULL);
-`;
